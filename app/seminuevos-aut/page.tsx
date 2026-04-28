18e0084b33073b8b1680a30c2165fb7af8e93910
Federico Lozano <flozano@grupodaytona.com>
2026-04-28 17:19:12 -0600

    Fix: Export default y ruta de página para clínica automática

--- app/seminuevos-aut/page.tsx
+++ app/seminuevos-aut/page.tsx
@@ -1,11 +1,5 @@
-'use client';
-
-import ClinicaSeminuevosSQL from '../../components/ClinicaSeminuevosSQL';
+import ClinicaSeminuevosSQL from '@/components/ClinicaSeminuevosSQL';
 
 export default function PaginaSoloClinica() {
-  return (
-    <div className="min-h-screen bg-slate-50">
-      <ClinicaSeminuevosSQL />
-    </div>
-  );
+  return <ClinicaSeminuevosSQL />;
 }
