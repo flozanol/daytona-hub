9f50ec0b58c964c3599829fca52a2ecf1b88f97d
Federico Lozano <flozano@grupodaytona.com>
2026-04-28 16:58:23 -0600

    Fix: Movida página fuera de api y corregido import

--- app/seminuevos-aut/page.tsx
+++ app/seminuevos-aut/page.tsx
@@ -1,5 +1,6 @@
+'use client'; // Agrega esto arriba para evitar errores de cliente/servidor
 import ClinicaSeminuevosSQL from '@/components/ClinicaSeminuevosSQL';
 
 export default function PaginaSoloClinica() {
-    return <ClinicaSeminuevosSQL />;
+  return <ClinicaSeminuevosSQL />;
 }
